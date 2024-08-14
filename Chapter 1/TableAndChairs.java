3fce65a77e0a023f3464e37e39d8080937e46fcc
Joemarie Jean Tibur <110674359+joems121@users.noreply.github.com>
2024-08-14 23:16:23 +0800

    Add files via upload

---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,13 @@
+public class TableAndChairs {
+   
+   public static void main(String[] args){ 
+   
+        System.out.println("x                            x");
+        System.out.println("x                            x");
+        System.out.println("x           xxxxxx           x");
+        System.out.println("x xxxx   x         x    xxxx x");
+        System.out.println("x     x  x         x  x      x");
+        System.out.println("x     x  x         x  x      x");
+   
+   }
+ }
