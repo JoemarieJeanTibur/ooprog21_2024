6c1fd46595ca3ba4657f06ba8f51f78b8fc5c5cc
Joemarie Jean Tibur <110674359+joems121@users.noreply.github.com>
2024-08-14 22:42:11 +0800

    Update MovieQuoteInfo.java

--- Chapter 1/MovieQuoteInfo.java
+++ Chapter 1/MovieQuoteInfo.java
@@ -18,22 +18,6 @@ public class MovieQuoteInfo {
     }
 }
 
-/*import javax.swing.JOptionPane;
-
-   public class MovieQuoteInfor {
-   
-      public static void main(String[] args) {
-      
-      String quote = "The music is all around us, all you have to do is listen.";
-      String movie = "August Rush";
-      String character = "Evan Taylor";
-      int year = 2007;
-      
-      String message = "Quote: " + quote + "\n" + "Movie: " + movie + "\n" + "Character" + character + "\n" +"Year: " + year;
-                       
-      JOptionPane.showMessageDialog(null, message, "Movie Quote", JOptionPane.INFORMATION_MESSAGE);
-    }
-}*/
 
                        
                          
