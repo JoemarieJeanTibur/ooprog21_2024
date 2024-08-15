e73c4416e2e8535a4971916a48f462d010e7768b
Joemarie Jean Tibur <110674359+joems121@users.noreply.github.com>
2024-08-16 02:53:18 +0800

    Update Triange.java

--- Chapter 1/Triange.java
+++ Chapter 1/Triange.java
@@ -1,18 +1,20 @@
+
 public class Triange {
+    
     public static void main(String[] args) {
-        int rows = 7;
-         
-         //rows for pyramid
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= rows - i; j++) {
+        for(int i=1; i<=7; i++){
+            
+            for(int a=1; a<=7-i; a++){
                 System.out.print(" ");
             }
-                  
-                  //  2 for double the T
-            for (int k = 1; k <= 2 * i - 1; k++) {
+            
+            for(int j=1; j<=i*2-1; j++){
                 System.out.print("T");
             }
-
-            System.out.println();         }
+            
+            System.out.println();
+            
+        }
     }
+    
 }
