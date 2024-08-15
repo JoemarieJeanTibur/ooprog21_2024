7a9f7222486382e429503999c9364ca051b096ef
Joemarie Jean Tibur <110674359+joems121@users.noreply.github.com>
2024-08-15 13:08:14 +0800

    Update and rename MovieQuoteInfo.java to MovieQuoteInfor.java

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,12 @@
+public class MovieQuoteInfor {
+     
+     public static void main(String[] args) {
+
+         System.out.println("Year:2007");
+         System.out.println("Movie: Augus Rush");
+         System.out.println("Quote: The music is all around us, all you have to do is listen.");
+         System.out.println("Character: Evan Taylor");
+         
+     }
+     
+}
