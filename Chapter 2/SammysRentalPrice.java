0f0a064d83f72a888be71ab1df7de14fc48d34d8
Joemarie Jean Tibur <110674359+JoemarieJeanTibur@users.noreply.github.com>
2024-09-02 21:22:47 +0800

    Update SammysRentalPrice.java

--- Chapter 2/SammysRentalPrice.java
+++ Chapter 2/SammysRentalPrice.java
@@ -8,10 +8,10 @@ public class SammysRentalPrice {
         System.out.println("S Sammy's make it fun in the sun. S");
         System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
 
-        Scanner event = new Scanner(System.in);
-        int rentMinutes;
+        Scanner tibur = new Scanner(System.in);
         System.out.print("Enter the number of minutes rented: ");
-        rentMinutes = event.nextInt();
+        int rentMinutes;
+        rentMinutes = tibur.nextInt();
 
         int hours = rentMinutes / 60;
         int extraMinutes = rentMinutes % 60;
@@ -23,7 +23,7 @@ public class SammysRentalPrice {
         System.out.println("Rental Cost: $" + rentalCost);
 
 
-    event.close();
+    tibur.close();
 
 
     }
