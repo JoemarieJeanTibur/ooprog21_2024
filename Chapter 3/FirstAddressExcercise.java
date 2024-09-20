a0763ff810019e79d84c97aaf83e6031d818f28b
Joemarie Jean Tibur <110674359+JoemarieJeanTibur@users.noreply.github.com>
2024-09-20 21:09:13 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,15 @@
+public class FirstAddressExcercise {
+
+    public static void main(String[] args) {
+        displayAddress();
+        System.out.println("First Java Application");
+
+    }
+
+    public static void displayAddress() {
+        System.out.println("Motorola Mobility");
+        System.out.println("LLC 222 W. Merchandise Mart Plaza, Suite 1800 ");
+        System.out.println("Chicago, Illinois 60654 USA");
+        System.out.println("Company: Motorola, Inc");
+    }
+}
